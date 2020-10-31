376cb56738c9354811806f7e7c8557c01e813d7c
Raja Gandharaw <Raja.Gandharaw@trivium-esolutions.com>
2020-10-31 11:45:29 +0530

    * Incorporated the review comments

--- src/app/common/constants/app-constants.ts
+++ src/app/common/constants/app-constants.ts
@@ -1,5 +1,5 @@
 export enum AppConstants {
-  APPLICATION_TITLE = 'Golang AppGenerator',
+  APPLICATION_TITLE = 'Golang AppGen',
   EXPLORE_DEFAULT_BUTTON_TEXT = 'Explore',
   PREPARING_BUTTON_TEXT = 'Preparing',
   CODE_SAND_BOX_URL = 'https://codesandbox.io/embed/github/gophers-prop/',
